9df8584744309a2ac5b7289022a5878a6d478a18
robi874 <62009463+robi874@users.noreply.github.com>
2021-04-13 19:04:21 +0200

    Added ItemGround prefab that can be used to make health potions

--- Assets/ItemPickupScript.cs
+++ Assets/ItemPickupScript.cs
@@ -8,6 +8,7 @@ public class ItemPickupScript : MonoBehaviour
 
     public ItemType itemType;
     public FloatVariable health;
+    public int healingValue = 25;
 
 
     // Start is called before the first frame update
@@ -26,18 +27,28 @@ public class ItemPickupScript : MonoBehaviour
     {
         switch (itemType)
         {
+           
             case ItemType.healthPotion:
-
+                Debug.Log(health.Value);
+                if(health.Value + healingValue > 100)
+                {
+                    health.Value = health.InitialValue;
+                }
+                else
+                {
+                    health.Value += healingValue;
+                }
                 break;
+          
         }
-        
-
+        Debug.Log(health.Value);
         Destroy(gameObject);
     }
 
     public enum ItemType
     {
-        healthPotion
+        healthPotion,
+        speedPotion
     }
 
 }
