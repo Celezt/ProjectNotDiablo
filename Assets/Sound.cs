409236ac496aa056eb3f5d05800c612eb6acfc4f
JeffPersson <Jeffepeffe@hotmail.com>
2021-04-22 11:12:00 +0200

    Impact sounds

---
+++ Assets/Sound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+//[SerializeField]
+public class Sound : MonoBehaviour
+{
+    //public string name;
+
+    //public AudioClip clip;
+
+    //[Range(0f,1f)]
+    //public float volume;
+    //[Range(0.1f, 3f)]
+    //public float pitch;
+
+    //[HideInInspector]
+    //public AudioSource source;
+}
