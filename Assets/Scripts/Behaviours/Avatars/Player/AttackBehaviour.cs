0da5ad788497a45f695c5079b9b3782f6cc8ea7b
Celezt <59172226+Celezt@users.noreply.github.com>
2021-05-06 16:06:00 +0200

    Updated AttackBehaviour

--- Assets/Scripts/Behaviours/Avatars/Player/AttackBehaviour.cs
+++ Assets/Scripts/Behaviours/Avatars/Player/AttackBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,12 @@ using MyBox;
 public class AttackBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject _selectedWeapon;
-
-    [SerializeField] private AnimationClip _attackClip;
-    [SerializeField] private float _animationSpeed = 1.0f;
-    [SerializeField] private float _stunMoveMultiplier = 1.0f;
-    [SerializeField] private float _stunDodgeMultiplier = 1.0f;
+    [SerializeField] private float _attackCombinationCooldown = 2.0f;
+    [Space(10)]
+    [SerializeField] private OrderType _meleeOrderType;
+    [SerializeField] private AttackData[] _meleeData;
+    [SerializeField] private OrderType _rangedOrderType;
+    [SerializeField] private AttackData[] _rangedData;
 
     [Foldout("Atoms", true)]
     [SerializeField] private Vector3Variable _pointWorldPositionVariable;
@@ -23,22 +25,91 @@ public class AttackBehaviour : MonoBehaviour
 
     private AnimatorBehaviour _animatorBehaviour;
 
+    private Duration _attackCombinationDuration;
+
+    private int _attackIndex;
+
+    [Serializable]
+    private struct AttackData
+    {
+        public AnimationClip[] Clip;
+        public float AnimationSpeedMultiplier;
+        public float StunMoveMultiplier;
+        public float StunDodgeMultiplier;
+    }
+
+    private enum OrderType
+    {
+        Random,
+        Sequence,
+    }
+
     #region Events
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if (_selectedWeapon.GetComponent<Ranged>() != null)
+        Melee melee = _selectedWeapon.GetComponent<Melee>();
+        if (melee != null)
         {
-            _selectedWeapon.GetComponent<Ranged>().Attack(_pointWorldPositionVariable.Value);
-        }
-        if (_selectedWeapon.GetComponent<Melee>() != null)
-        {
-            _selectedWeapon.GetComponent<Melee>().Attack(transform);
+            melee.Attack(transform);
 
-            if (_animatorBehaviour.IsAnimationModifierRunning == false)
+            if (!_animatorBehaviour.IsAnimationModifierRunning)
             {
-                _stunDodgeList.Add(new Duration(_attackClip.length / _animationSpeed * _stunDodgeMultiplier));
-                _stunMoveList.Add(new Duration(_attackClip.length / _animationSpeed * _stunMoveMultiplier));
-                _animatorBehaviour.OnAnimationModifierRaised(new AnimatorModifier(_attackClip, _animationSpeed));
+                int index = 0;
+
+                if (_meleeOrderType == OrderType.Random)
+                    index = UnityEngine.Random.Range(0, _meleeData.Length);
+                else if (_meleeOrderType == OrderType.Sequence)
+                {
+                    if (!_attackCombinationDuration.IsActive)
+                        _attackIndex = 0;
+
+                    index = _attackIndex;
+
+                    _attackIndex = (_attackIndex + 1) % _meleeData.Length;
+
+                    _attackCombinationDuration = new Duration(_attackCombinationCooldown);
+                }
+
+                AttackData data = _meleeData[index];
+
+                int clipIndex = UnityEngine.Random.Range(0, data.Clip.Length);  // Pick random clip.
+
+                _stunDodgeList.Add(new Duration(data.Clip[clipIndex].length / data.AnimationSpeedMultiplier * data.StunDodgeMultiplier));
+                _stunMoveList.Add(new Duration(data.Clip[clipIndex].length / data.AnimationSpeedMultiplier * data.StunMoveMultiplier));
+                _animatorBehaviour.OnAnimationModifierRaised(new AnimatorModifier(data.Clip[clipIndex], data.AnimationSpeedMultiplier));
+            }
+        }
+
+        Ranged ranged = _selectedWeapon.GetComponent<Ranged>();
+        if (ranged != null)
+        {
+            ranged.Attack(_pointWorldPositionVariable.Value);
+
+            if (!_animatorBehaviour.IsAnimationModifierRunning)
+            {
+                int index = 0;
+
+                if (_rangedOrderType == OrderType.Random)
+                    index = UnityEngine.Random.Range(0, _rangedData.Length);
+                else if (_rangedOrderType == OrderType.Sequence)
+                {
+                    if (!_attackCombinationDuration.IsActive)
+                        _attackIndex = 0;
+
+                    index = _attackIndex;
+
+                    _attackIndex = (_attackIndex + 1) % _rangedData.Length;
+
+                    _attackCombinationDuration = new Duration(_attackCombinationCooldown);
+                }
+
+                AttackData data = _rangedData[index];
+
+                int clipIndex = UnityEngine.Random.Range(0, data.Clip.Length);  // Pick random clip.
+
+                _stunDodgeList.Add(new Duration(data.Clip[clipIndex].length / data.AnimationSpeedMultiplier * data.StunDodgeMultiplier));
+                _stunMoveList.Add(new Duration(data.Clip[clipIndex].length / data.AnimationSpeedMultiplier * data.StunMoveMultiplier));
+                _animatorBehaviour.OnAnimationModifierRaised(new AnimatorModifier(data.Clip[clipIndex], data.AnimationSpeedMultiplier));
             }
         }
     }
