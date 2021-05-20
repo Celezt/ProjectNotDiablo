0dffea6d7e32e22f7739c59e3bbbd8544d03f6ae
Celezt <59172226+Celezt@users.noreply.github.com>
2021-05-20 14:29:42 +0200

    Updated TakeDamage and added popup damage

---
+++ Assets/Scripts/Behaviours/Avatars/Player/PlayerDataReference.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityAtoms.BaseAtoms;
+
+public class PlayerDataReference : MonoBehaviour
+{
+    public FloatVariable Health;
+    public FloatVariable MaxHealth;
+    public FloatVariable MovementSpeed;
+}
