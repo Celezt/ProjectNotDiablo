0c684a11209c2a9347b7a16786e38c31244fd1cd
Celezt <59172226+Celezt@users.noreply.github.com>
2021-05-20 14:30:04 +0200

    Updated TakeDamage and added popup damage (#61)
    * Improved menu controller support
    
    * Improved cursor
    
    * Improved gamepad support and updated InputSystem
    
    * Added key icons
    
    * Reorganized project and added key icons
    
    * Updated AttackBehaviour
    
    * Updated selected weapon in AttackBehaviour
    
    * Update AttackBehaviour
    
    * Fixed stuff
    
    * Fixed cursor
    
    * Removed folder
    
    * Fixed scaling issues
    
    * Updated AI animation and removed sample
    
    * Updated TakeDamage and added popup damage

--- Assets/Scripts/Combat/TakeDamage.cs
+++ Assets/Scripts/Combat/TakeDamage.cs
@@ -2,28 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityAtoms.BaseAtoms;
+using UnityEngine.UI;
+
 public class TakeDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField]
-    FloatVariable variable;
+    [SerializeField] private GameObject _popup;
+    [SerializeField] private float _randomPopupOffset = 20;
+    [SerializeField] private Vector3 _offsetPopup;
 
     public void ReciveDamage(float damage)
     {
-        if (gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (gameObject.layer == LayerMask.NameToLayer("Player"))    // Player hit logic.
         {
-            //player Hit logic
-            variable.Value -= damage;
-            Debug.Log("OW-EEE");
+            gameObject.GetComponent<PlayerDataReference>().Health.Value -= damage;
         }
-        else if (gameObject.layer == LayerMask.NameToLayer("AI"))
+        else if (gameObject.layer == LayerMask.NameToLayer("AI"))   // AI hit logic.
         {
-            Debug.Log("CLANG");
             gameObject.GetComponent<AI>().health.Value -= damage;
         }
         else if (gameObject.layer == LayerMask.NameToLayer("Damageble"))
         {
 
+        }
+
+        if (_popup != null)
+        {
+            Vector3 position = transform.position;
+            GameObject instance = Instantiate(_popup, new Vector3(
+                _offsetPopup.x + Random.Range(position.x - _randomPopupOffset, position.x + _randomPopupOffset),
+                _offsetPopup.y + Random.Range(position.y - _randomPopupOffset, position.y + _randomPopupOffset),
+                _offsetPopup.z + Random.Range(position.z - _randomPopupOffset, position.z + _randomPopupOffset)),
+                Quaternion.identity);
+            Text text = instance.GetComponentInChildren<Text>();
+            text.text = damage.ToString();
         }
     }
 }
